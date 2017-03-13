579362ba2df6a130ecd1f21855a29384d99b014e
DejanAcim <dejanacim@gmail.com>
2017-03-13 14:46:50 +0100

    Add users to articles but one RSpec error persists

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312113942) do
+ActiveRecord::Schema.define(version: 20170313120223) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
